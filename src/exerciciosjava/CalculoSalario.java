06476d41e5a2b7a25f8ad94f67bc9a9702755536
Gabriela Guerra <gabrielaguerra438@gmail.com>
2023-05-24 15:17:08 -0300

    @GabrielaFigueiredoDev Primeiras atividades práticas em Java

---
+++ src/exerciciosjava/CalculoSalario.java
@@ -0,0 +1,25 @@
+package exerciciosjava;
+
+import java.util.Scanner;
+
+public class CalculoSalario {
+
+	public static void main(String[] args) {
+		
+		float salario, abono, novosalario;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.print("Digite o seu salário: ");
+		salario = leia.nextFloat();
+		
+		System.out.print("Digite o seu abono: ");
+		abono = leia.nextFloat();
+		
+		novosalario = salario + abono;
+		
+		System.out.print("\nSeu salário é: " + novosalario);
+
+	}
+
+}
---
+++ src/exerciciosjava/CalculoSalario2.java
@@ -0,0 +1,30 @@
+package exerciciosjava;
+
+import java.util.Scanner;
+
+public class CalculoSalario2 {
+
+	public static void main(String[] args) {
+		float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.print("Digite o valor do seu salário bruto: ");
+		salarioBruto = leia.nextFloat();
+		
+		System.out.print("Digite o valor recebido pelo adicional noturno: ");
+		adicionalNoturno = leia.nextFloat();
+		
+		System.out.print("Digite o valor recebido pelas horas extras: ");
+		horasExtras = leia.nextFloat();
+		
+		System.out.print("Digite o valor de que foi descontado: ");
+		descontos = leia.nextFloat();
+		
+		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5 ) - descontos;
+		
+		System.out.print("\nSeu salário liquido é: " + salarioLiquido);
+
+	}
+
+}
